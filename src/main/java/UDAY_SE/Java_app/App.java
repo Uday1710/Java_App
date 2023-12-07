a5273245092029b32c1d528ba8897315724824bf
Uday1710 <ondrathi.uday@gmail.com>
2023-12-07 20:02:07 +0530

    maven java push

---
+++ src/main/java/UDAY_SE/Java_app/App.java
@@ -0,0 +1,13 @@
+package UDAY_SE.Java_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
